3ca3dd1abc07ac7e163fcc931bbebec290bf2572
jinwoo.shin <jw0227.shin@samsung.com>
2017-08-02 20:19:21 +0900

    Add initial project
    Signed-off-by: jinwoo.shin <jw0227.shin@samsung.com>

---
+++ XamarinMediaPlayer/XamarinMediaPlayer.Droid/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+namespace XamarinMediaPlayer.Droid
+{
+    [Activity(Label = "XamarinMediaPlayer.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
+
