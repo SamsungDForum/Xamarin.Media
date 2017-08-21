979571bd78b8e05844add5766bad782ce12b2f40
Minkyu Kang <mk7.kang@samsung.com>
2017-08-21 15:56:46 +0900

    Android: add custom renderer for disabling scrollbar
    Signed-off-by: Minkyu Kang <mk7.kang@samsung.com>

---
+++ XamarinMediaPlayer/XamarinMediaPlayer.Droid/Renderer/ContentListRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinMediaPlayer.Controls;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ContentList), typeof(ContentListRenderer))]
+namespace XamarinMediaPlayer.Controls
+{
+    public class ContentListRenderer : ScrollViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            e.NewElement.PropertyChanged += OnElementPropertyChanged;
+        }
+
+        protected void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (ChildCount > 0)
+            {
+                GetChildAt(0).HorizontalScrollBarEnabled = false;
+                GetChildAt(0).VerticalScrollBarEnabled = false;
+            }
+        }
+    }
+}
